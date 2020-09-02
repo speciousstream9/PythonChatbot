fe7f5d013811728bc41df3f2552480cfd319201b
speciousstream9 <speciouscodabishop@gmail.com>
2020-09-02 19:26:23 +0100

    Create chatbot.py
    0.0.1

---
+++ chatbot.py
@@ -0,0 +1,4 @@
+print("Hi there! Welcome to SpecBot")
+print("I'm not fully optomised yet, right now I can just echo things back to you. But I hope to be able to chat to you really soon!")
+stuff_to_echo = input("Enter something for me to repeat!")
+print("You said:" + stuff_to_echo)
